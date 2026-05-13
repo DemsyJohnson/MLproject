bed90eb489024e14480b1825e1d53aa45e512999
Udeme Johnson <demsyclim@gmail.com>
2026-05-13 05:23:19 +0100

    another EDA

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,1 +1,13 @@
-# To read the data
+# To read the data from the database or live stream
+
+import os
+import sys
+from src.exception import CustomExceptions
+from src.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+# To create class variables.
+from dataclasses import dataclass
+
